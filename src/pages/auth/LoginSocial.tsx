d0b098ef9ce3ec405302d9448bbf703c74c4b1c6
elecharu <elec_haru@naver.com>
2025-09-30 16:28:54 +0900

    supabase setting update

--- src/pages/auth/LoginSocial.tsx
+++ src/pages/auth/LoginSocial.tsx
@@ -1,13 +1,23 @@
 import { Link } from "react-router";
 import { Github } from "lucide-react";
+import supabase from "../../utils/supabase";
 
 export default function LoginSocial() {
   const handleGoogleLogin = () => {
     console.log("Google login");
   };
 
-  const handleGithubLogin = () => {
+  const handleGithubLogin = async () => {
     console.log("Github login");
+    try {
+      const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: "github",
+      });
+      console.log(data);
+      if (error) throw error;
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   const handleKakaoLogin = () => {
